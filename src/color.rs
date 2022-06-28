5e932f9dbb0fc144b1bb8ac1fe8c1d0fa8df9e40
Vargnath <15263997+Vargnath@users.noreply.github.com>
2022-06-28 18:17:50 +0200

    Implement color operations

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,6 @@
+use crate::float_eq;
+use std::ops::{Add, Mul, Sub};
+
 #[derive(Debug, Copy, Clone)]
 pub struct Color {
     pub red: f64,
@@ -8,6 +11,62 @@ pub struct Color {
 impl Color {
     pub fn new(red: f64, green: f64, blue: f64) -> Self {
         Color { red, green, blue }
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Self) -> bool {
+        float_eq(self.red, other.red)
+            && float_eq(self.green, other.green)
+            && float_eq(self.blue, other.blue)
+    }
+}
+
+impl Add for Color {
+    type Output = Color;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            red: self.red + rhs.red,
+            green: self.green + rhs.green,
+            blue: self.blue + rhs.blue,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Color;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            red: self.red - rhs.red,
+            green: self.green - rhs.green,
+            blue: self.blue - rhs.blue,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Color;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self::Output {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+
+impl Mul for Color {
+    type Output = Color;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            red: self.red * rhs.red,
+            green: self.green * rhs.green,
+            blue: self.blue * rhs.blue,
+        }
     }
 }
 
@@ -22,5 +81,36 @@ mod tests {
         assert!(float_eq(c.red, -0.5));
         assert!(float_eq(c.green, 0.4));
         assert!(float_eq(c.blue, 1.7));
+    }
+
+    #[test]
+    fn adding_colors() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        let expected = Color::new(1.6, 0.7, 1.0);
+        assert_eq!(c1 + c2, expected);
+    }
+
+    #[test]
+    fn subtracting_colors() {
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        let expected = Color::new(0.2, 0.5, 0.5);
+        assert_eq!(c1 - c2, expected);
+    }
+
+    #[test]
+    fn multiplying_a_color_by_a_scalar() {
+        let c = Color::new(0.2, 0.3, 0.4);
+        let expected = Color::new(0.4, 0.6, 0.8);
+        assert_eq!(c * 2.0, expected);
+    }
+
+    #[test]
+    fn multiplying_colors() {
+        let c1 = Color::new(1.0, 0.2, 0.4);
+        let c2 = Color::new(0.9, 1.0, 0.1);
+        let expected = Color::new(0.9, 0.2, 0.04);
+        assert_eq!(c1 * c2, expected);
     }
 }
