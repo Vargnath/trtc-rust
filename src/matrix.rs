e0dba418230ba126929f7e374d0ae9ee1a04dad0
Vargnath <15263997+Vargnath@users.noreply.github.com>
2022-07-08 19:29:12 +0200

    Implement matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,17 +1,18 @@
 use crate::float_eq;
-use std::ops::{Index, IndexMut};
+use crate::tuple::Tuple;
+use std::ops::{Index, IndexMut, Mul};
 
-#[derive(Debug)]
+#[derive(Debug, Default, Copy, Clone)]
 pub struct Matrix2 {
     rows: [[f64; 2]; 2],
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default, Copy, Clone)]
 pub struct Matrix3 {
     rows: [[f64; 3]; 3],
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default, Copy, Clone)]
 pub struct Matrix4 {
     rows: [[f64; 4]; 4],
 }
@@ -47,6 +48,23 @@ macro_rules! impl_matrix {
                 &mut self.rows[index]
             }
         }
+
+        impl Mul for $MatrixN {
+            type Output = $MatrixN;
+
+            fn mul(self, rhs: Self) -> Self::Output {
+                let mut result = $MatrixN::default();
+                for i in 0..$n {
+                    for j in 0..$n {
+                        let element = &mut result[i][j];
+                        for k in 0..$n {
+                            *element += self.rows[i][k] * rhs.rows[k][j];
+                        }
+                    }
+                }
+                result
+            }
+        }
     };
 }
 
@@ -54,10 +72,24 @@ impl_matrix!(Matrix2, 2);
 impl_matrix!(Matrix3, 3);
 impl_matrix!(Matrix4, 4);
 
+impl Mul<Tuple> for Matrix4 {
+    type Output = Tuple;
+
+    fn mul(self, rhs: Tuple) -> Self::Output {
+        Tuple::new(
+            self[0][0] * rhs.x + self[0][1] * rhs.y + self[0][2] * rhs.z + self[0][3] * rhs.w,
+            self[1][0] * rhs.x + self[1][1] * rhs.y + self[1][2] * rhs.z + self[1][3] * rhs.w,
+            self[2][0] * rhs.x + self[2][1] * rhs.y + self[2][2] * rhs.z + self[2][3] * rhs.w,
+            self[3][0] * rhs.x + self[3][1] * rhs.y + self[3][2] * rhs.z + self[3][3] * rhs.w,
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::float_eq;
     use crate::matrix::{Matrix2, Matrix3, Matrix4};
+    use crate::tuple::Tuple;
 
     #[test]
     fn constructing_and_inspecting_a_2_x_2_matrix() {
@@ -130,5 +162,43 @@ mod tests {
         ]);
 
         assert_ne!(a, b);
+    }
+
+    #[test]
+    fn multiplying_two_matrices() {
+        let a = Matrix4::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 8.0, 7.0, 6.0],
+            [5.0, 4.0, 3.0, 2.0],
+        ]);
+        let b = Matrix4::new([
+            [-2.0, 1.0, 2.0, 3.0],
+            [3.0, 2.0, 1.0, -1.0],
+            [4.0, 3.0, 6.0, 5.0],
+            [1.0, 2.0, 7.0, 8.0],
+        ]);
+        let expected = Matrix4::new([
+            [20.0, 22.0, 50.0, 48.0],
+            [44.0, 54.0, 114.0, 108.0],
+            [40.0, 58.0, 110.0, 102.0],
+            [16.0, 26.0, 46.0, 42.0],
+        ]);
+
+        assert_eq!(a * b, expected);
+    }
+
+    #[test]
+    fn a_matrix_multiplied_by_a_tuple() {
+        let a = Matrix4::new([
+            [1.0, 2.0, 3.0, 4.0],
+            [2.0, 4.0, 4.0, 2.0],
+            [8.0, 6.0, 4.0, 1.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+        let b = Tuple::new(1.0, 2.0, 3.0, 1.0);
+        let expected = Tuple::new(18.0, 24.0, 33.0, 1.0);
+
+        assert_eq!(a * b, expected);
     }
 }
--- src/tuple.rs
+++ src/tuple.rs
@@ -6,10 +6,14 @@ pub struct Tuple {
     pub x: f64,
     pub y: f64,
     pub z: f64,
-    w: f64,
+    pub w: f64,
 }
 
 impl Tuple {
+    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+        Self { x, y, z, w }
+    }
+
     pub fn new_point(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z, w: 1.0 }
     }
