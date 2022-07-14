d08efd3b7aaca74c28a2c0ffa4fdc2fdd90bb879
Vargnath <15263997+Vargnath@users.noreply.github.com>
2022-07-14 23:53:08 +0200

    Implement ray and sphere transformation

--- src/ray.rs
+++ src/ray.rs
@@ -1,3 +1,4 @@
+use crate::matrix::Matrix4;
 use crate::tuple::Tuple;
 
 #[derive(Debug, Copy, Clone)]
@@ -14,10 +15,15 @@ impl Ray {
     pub fn position(&self, t: f64) -> Tuple {
         self.origin + self.direction * t
     }
+
+    pub fn transform(&self, matrix: Matrix4) -> Self {
+        Self::new(matrix * self.origin, matrix * self.direction)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::matrix::Matrix4;
     use crate::ray::Ray;
     use crate::tuple::Tuple;
 
@@ -49,5 +55,37 @@ mod tests {
 
         let expected = Tuple::new_point(4.5, 3.0, 4.0);
         assert_eq!(r.position(2.5), expected);
+    }
+
+    #[test]
+    fn translating_a_ray() {
+        let r = Ray::new(
+            Tuple::new_point(1.0, 2.0, 3.0),
+            Tuple::new_vector(0.0, 1.0, 0.0),
+        );
+        let m = Matrix4::translation(3.0, 4.0, 5.0);
+        let r2 = r.transform(m);
+
+        let expected = Tuple::new_point(4.0, 6.0, 8.0);
+        assert_eq!(r2.origin, expected);
+
+        let expected = Tuple::new_vector(0.0, 1.0, 0.0);
+        assert_eq!(r2.direction, expected);
+    }
+
+    #[test]
+    fn scaling_a_ray() {
+        let r = Ray::new(
+            Tuple::new_point(1.0, 2.0, 3.0),
+            Tuple::new_vector(0.0, 1.0, 0.0),
+        );
+        let m = Matrix4::scaling(2.0, 3.0, 4.0);
+        let r2 = r.transform(m);
+
+        let expected = Tuple::new_point(2.0, 6.0, 12.0);
+        assert_eq!(r2.origin, expected);
+
+        let expected = Tuple::new_vector(0.0, 3.0, 0.0);
+        assert_eq!(r2.direction, expected);
     }
 }
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,16 +1,22 @@
 use crate::intersections::{Intersection, Intersections};
+use crate::matrix::Matrix4;
 use crate::ray::Ray;
 use crate::tuple::Tuple;
 
 #[derive(Debug, Default, Copy, Clone)]
-pub struct Sphere {}
+pub struct Sphere {
+    pub transform: Matrix4,
+}
 
 impl Sphere {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            transform: Matrix4::identity(),
+        }
     }
 
     pub fn intersect(&self, ray: Ray) -> Intersections {
+        let ray = ray.transform(self.transform.inverse());
         let sphere_to_ray = ray.origin - Tuple::new_point(0.0, 0.0, 0.0);
         let a = ray.direction * ray.direction;
         let b = 2.0 * (ray.direction * sphere_to_ray);
@@ -31,6 +37,7 @@ impl Sphere {
 #[cfg(test)]
 mod tests {
     use crate::assert_float_eq;
+    use crate::matrix::Matrix4;
     use crate::ray::Ray;
     use crate::sphere::Sphere;
     use crate::tuple::Tuple;
@@ -111,5 +118,49 @@ mod tests {
         assert_eq!(xs.len(), 2);
         assert!(ptr::eq(xs[0].object, &s));
         assert!(ptr::eq(xs[1].object, &s));
+    }
+
+    #[test]
+    fn a_spheres_default_transformation() {
+        let s = Sphere::new();
+
+        assert_eq!(s.transform, Matrix4::identity());
+    }
+
+    #[test]
+    fn changing_a_spheres_transformation() {
+        let mut s = Sphere::new();
+        let t = Matrix4::translation(2.0, 3.0, 4.0);
+        s.transform = t;
+
+        assert_eq!(s.transform, t);
+    }
+
+    #[test]
+    fn intersecting_a_scaled_sphere_with_a_ray() {
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, -5.0),
+            Tuple::new_vector(0.0, 0.0, 1.0),
+        );
+        let mut s = Sphere::new();
+        s.transform = Matrix4::scaling(2.0, 2.0, 2.0);
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_float_eq!(xs[0].t, 3.0);
+        assert_float_eq!(xs[1].t, 7.0);
+    }
+
+    #[test]
+    fn intersecting_a_translated_sphere_with_a_ray() {
+        let r = Ray::new(
+            Tuple::new_point(0.0, 0.0, -5.0),
+            Tuple::new_vector(0.0, 0.0, 1.0),
+        );
+        let mut s = Sphere::new();
+        s.transform = Matrix4::translation(5.0, 0.0, 0.0);
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 0);
     }
 }
